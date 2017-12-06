8db80983d2b77d093a2c2a66b76b28cf4660a156
Patrick Pokatilo <mail@shyxormz.net>
2017-12-06 15:08:51 +0100

    Solution to day 2, second half

--- cmd/aoc/checksum.go
+++ cmd/aoc/checksum.go
@@ -44,18 +44,59 @@ func Checksum(data []byte) (int, error) {
 	return sum, nil
 }
 
+func ChecksumDivision(data []byte) (int, error) {
+	sum := 0
+	for len(data) > 0 {
+		ns := make([]int, 0)
+		for {
+			i := bytes.IndexAny(data, "\t \n")
+			if i < 1 {
+				break
+			}
+			n, err := strconv.Atoi(string(data[0:i]))
+			if err != nil {
+				return 0, err
+			}
+			ns = append(ns, n)
+			for data[i] == '\t' || data[i] == ' ' {
+				i++
+			}
+			data = data[i:]
+		}
+		for ni, nl := range ns {
+			for _, nr := range ns[(ni+1):] {
+				if nl % nr == 0 {
+					sum += nl / nr
+					break
+				}
+				if nr % nl == 0 {
+					sum += nr / nl
+					break
+				}
+			}
+		}
+
+		i := bytes.IndexAny(data, "\n")
+		if i != 0 {
+			break
+		}
+		data = data[1:]
+	}
+	return sum, nil
+}
+
 func checksumCommand() error {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		panic("not enough arguments")
 	}
 
 	var f *os.File
 	var err error
 
-	if os.Args[2] == "-" {
+	if os.Args[3] == "-" {
 		f = os.Stdin
 	} else {
-		f, err = os.OpenFile(os.Args[2], os.O_RDONLY, 0755)
+		f, err = os.OpenFile(os.Args[3], os.O_RDONLY, 0755)
 		if err != nil {
 			return err
 		}
@@ -68,7 +109,17 @@ func checksumCommand() error {
 	}
 	c = bytes.TrimSpace(c)
 
-	sum, err := Checksum(c)
+	var sum int
+
+	switch os.Args[2] {
+	case "minmax":
+		sum, err = Checksum(c)
+	case "division":
+		sum, err = ChecksumDivision(c)
+	default:
+		panic("unknown sub-command: " + os.Args[2])
+	}
+
 	if err != nil {
 		return err
 	}
--- cmd/aoc/checksum_test.go
+++ cmd/aoc/checksum_test.go
@@ -8,3 +8,10 @@ func TestChecksum(t *testing.T) {
 		t.Errorf("got (%d, %+v), want (%d, %+v)", r, err, 18, nil)
 	}
 }
+
+func TestChecksumDivision(t *testing.T) {
+	r, err := ChecksumDivision([]byte("5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5"))
+	if r != 9 || err != nil {
+		t.Errorf("got (%d, %+v), want (%d, %+v)", r, err, 9, nil)
+	}
+}
