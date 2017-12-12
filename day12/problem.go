5676362304c2a9dad475d375df5ca0f0047ff092
Patrick <mail@shyxormz.net>
2017-12-12 11:09:21 +0100

    Solution to day 12, second half

--- day12/problem.go
+++ day12/problem.go
@@ -2,6 +2,7 @@ package day12
 
 import (
 	"bytes"
+	"container/ring"
 	"fmt"
 	"strconv"
 )
@@ -12,7 +13,55 @@ func Problem() *problem {
 	return &problem{}
 }
 
-func (problem) PartOne(data []byte) (string, error) {
+func (p problem) PartOne(data []byte) (string, error) {
+	ps, err := p.parse(data)
+	if err != nil {
+		return "", err
+	}
+	rs := p.construct(ps)
+	return fmt.Sprintf("%d", rs[0].Len()), nil
+}
+
+func (p problem) PartTwo(data []byte) (string, error) {
+	ps, err := p.parse(data)
+	if err != nil {
+		return "", err
+	}
+	rs := p.construct(ps)
+	for _, r := range rs {
+		for i := r.Len() - 1; i > 0; i-- {
+			r = r.Next()
+			delete(rs, r.Value.(program).ID)
+		}
+	}
+	return fmt.Sprintf("%d", len(rs)), nil
+}
+
+func (problem) construct(ps []program) map[int]*ring.Ring {
+	rs := make(map[int]*ring.Ring)
+	for _, p := range ps {
+		rs[p.ID] = &ring.Ring{
+			Value: p,
+		}
+	}
+	for _, p := range ps {
+		for _, i := range p.Pipes {
+			r := rs[p.ID]
+			c := false
+			r.Do(func(v interface{}) {
+				if v.(program).ID == i {
+					c = true
+				}
+			})
+			if !c {
+				r.Link(rs[i])
+			}
+		}
+	}
+	return rs
+}
+
+func (problem) parse(data []byte) ([]program, error) {
 	var ps []program
 	for _, line := range bytes.Split(data, []byte("\n")) {
 		if len(line) == 0 {
@@ -23,47 +72,20 @@ func (problem) PartOne(data []byte) (string, error) {
 		for _, n := range bytes.Split(parts[1], []byte(", ")) {
 			id, err := strconv.Atoi(string(n))
 			if err != nil {
-				return "", err
+				return nil, err
 			}
 			ids = append(ids, id)
 		}
 		id, err := strconv.Atoi(string(parts[0]))
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 		ps = append(ps, program{
 			ID:    id,
 			Pipes: ids,
 		})
 	}
-	cy := set{M: make(map[int]struct{})}
-	cn := set{M: make(map[int]struct{})}
-	for _, p := range ps {
-		cn.Add(p.ID)
-	}
-	mod := true
-	for mod {
-		mod = false
-		for _, p := range ps {
-			if p.ID == 0 && cn.Contains(p.ID) {
-				cn.Delete(p.ID)
-				cy.Add(p.ID)
-				mod = true
-			}
-			for _, id := range p.Pipes {
-				if cy.Contains(id) && cn.Contains(p.ID) {
-					cn.Delete(p.ID)
-					cy.Add(p.ID)
-					mod = true
-				}
-			}
-		}
-	}
-	return fmt.Sprintf("%d", len(cy.M)), nil
-}
-
-func (problem) PartTwo(data []byte) (string, error) {
-	return "", nil
+	return ps, nil
 }
 
 type program struct {
--- day12/problem_test.go
+++ day12/problem_test.go
@@ -11,3 +11,10 @@ func TestProblem_PartOne(t *testing.T) {
 		t.Errorf("got (%#v, %+v), want (%#v, %+v)", r, err, "6", nil)
 	}
 }
+
+func TestProblem_PartTwo(t *testing.T) {
+	r, err := day12.Problem().PartTwo([]byte("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5\n"))
+	if r != "2" || err != nil {
+		t.Errorf("got (%#v, %+v), want (%#v, %+v)", r, err, "2", nil)
+	}
+}
