a0e4cb874eb447e3f767939eaa75656d436ba125
Patrick Pokatilo <mail@shyxormz.net>
2018-11-26 16:30:03 +0100

    Add solution to 2015/01

---
+++ cmd/aoc-2015/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/SHyx0rmZ/advent-of-code"
+	"github.com/SHyx0rmZ/advent-of-code/pkg/input"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day01"
+)
+
+var problems = []aoc.ReaderAwareProblem{
+	day01.Problem(),
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		usage()
+	}
+
+	args := struct {
+		Day       string
+		Challenge string
+		Input     string
+	}{
+		Day:       os.Args[1],
+		Challenge: os.Args[2],
+	}
+
+	if len(os.Args) > 3 {
+		args.Input = os.Args[3]
+	}
+
+	problem, err := strconv.Atoi(args.Day)
+	if err != nil {
+		panic(err)
+	}
+
+	if args.Input == "" {
+		path := fmt.Sprintf("year2015/day%02d/data/input.txt", problem)
+		f, err := os.Open(path)
+		if err != nil {
+			usage()
+		}
+		f.Close()
+		args.Input = path
+	}
+
+	problem--
+
+	p := problems[problem]
+
+	var answer string
+
+	answer, err = solveReaderAwareProblem(p, args.Challenge, args.Input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s\n", answer)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s <01-24> <a|b> <input>\n", filepath.Base(os.Args[0]))
+	os.Exit(1)
+}
+
+func solveReaderAwareProblem(problem aoc.ReaderAwareProblem, challenge, path string) (string, error) {
+	r, err := input.OpenInputFile(path)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	switch challenge {
+	case "a":
+		return problem.PartOneWithReader(r)
+	case "b":
+		return problem.PartTwoWithReader(r)
+	}
+	panic("expect either 'a' or 'b'")
+}
---
+++ year2015/day01/command.go
@@ -0,0 +1,53 @@
+package day01
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/SHyx0rmZ/advent-of-code"
+	"io"
+)
+
+type problem struct{}
+
+func Problem() aoc.ReaderAwareProblem {
+	return &problem{}
+}
+
+func (p problem) PartOneWithReader(r io.Reader) (string, error) {
+	var i int
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanRunes)
+	for s.Scan() {
+		switch s.Text() {
+		case "(":
+			i++
+		case ")":
+			i--
+		default:
+			panic("unexpected")
+		}
+	}
+	return fmt.Sprintf("%d", i), s.Err()
+}
+
+func (p problem) PartTwoWithReader(r io.Reader)  (string, error) {
+	var i int
+	var j int
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanRunes)
+	for s.Scan() {
+		j++
+		switch s.Text() {
+		case "(":
+			i++
+		case ")":
+			i--
+			if i < 0 {
+				return fmt.Sprintf("%d", j), s.Err()
+			}
+		default:
+			panic("unexpected")
+		}
+	}
+	panic("unexpected")
+}
