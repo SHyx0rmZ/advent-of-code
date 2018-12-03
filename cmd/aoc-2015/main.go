3effdb21e7c2dbd966975523c05c70cfd0b493e0
Patrick Pokatilo <mail@shyxormz.net>
2018-12-03 22:14:34 +0100

    Add remaining days for 2015

--- cmd/aoc-2015/main.go
+++ cmd/aoc-2015/main.go
@@ -15,6 +15,25 @@ import (
 	"github.com/SHyx0rmZ/advent-of-code/year2015/day04"
 	"github.com/SHyx0rmZ/advent-of-code/year2015/day05"
 	"github.com/SHyx0rmZ/advent-of-code/year2015/day06"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day07"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day08"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day09"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day10"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day11"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day12"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day13"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day14"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day15"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day16"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day17"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day18"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day19"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day20"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day21"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day22"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day23"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day24"
+	"github.com/SHyx0rmZ/advent-of-code/year2015/day25"
 )
 
 var problems = []aoc.ReaderAwareProblem{
@@ -24,6 +43,25 @@ var problems = []aoc.ReaderAwareProblem{
 	day04.Problem(),
 	day05.Problem(),
 	day06.Problem(),
+	day07.Problem(),
+	day08.Problem(),
+	day09.Problem(),
+	day10.Problem(),
+	day11.Problem(),
+	day12.Problem(),
+	day13.Problem(),
+	day14.Problem(),
+	day15.Problem(),
+	day16.Problem(),
+	day17.Problem(),
+	day18.Problem(),
+	day19.Problem(),
+	day20.Problem(),
+	day21.Problem(),
+	day22.Problem(),
+	day23.Problem(),
+	day24.Problem(),
+	day25.Problem(),
 }
 
 func main() {
