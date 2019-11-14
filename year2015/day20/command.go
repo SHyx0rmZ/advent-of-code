09f45d8ef86bc244490f823be2b4361403758944
Patrick Pokatilo <mail@shyxormz.net>
2019-11-14 20:33:12 +0100

    2015:20

--- year2015/day20/command.go
+++ year2015/day20/command.go
@@ -1,7 +1,9 @@
 package day20
 
 import (
+	"bufio"
 	"io"
+	"strconv"
 
 	"github.com/SHyx0rmZ/advent-of-code"
 )
@@ -14,9 +16,47 @@ func Problem() aoc.ReaderAwareProblem {
 }
 
 func (p problem) PartOneWithReader(r io.Reader) (string, error) {
-	return "", nil
+	s, err := bufio.NewReader(r).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	n, err := strconv.Atoi(s[:len(s)-1])
+	if err != nil {
+		return "", err
+	}
+	m := make(map[int]int, n)
+	for i := 1; i < n; i++ {
+		for j := i; j < n; j += i {
+			m[j] += i * 10
+		}
+		p := m[i]
+		//fmt.Println("House", i, "got", p, "presents")
+		if p >= n {
+			return strconv.Itoa(i), nil
+		}
+	}
+	return "?", nil
 }
 
 func (p problem) PartTwoWithReader(r io.Reader) (string, error) {
-	return "", nil
+	s, err := bufio.NewReader(r).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	n, err := strconv.Atoi(s[:len(s)-1])
+	if err != nil {
+		return "", err
+	}
+	m := make(map[int]int, n)
+	for i := 1; i < n; i++ {
+		for j, k := i, 1; k < 51; j, k = j + i, k + 1 {
+			m[j] += i * 11
+		}
+		p := m[i]
+		//fmt.Println("House", i, "got", p, "presents")
+		if p >= n {
+			return strconv.Itoa(i), nil
+		}
+	}
+	return "?", nil
 }
