040d00ddf1e3f6a2e91d74b6333614177bf59726
Patrick Pokatilo <mail@shyxormz.net>
2019-12-16 18:06:30 +0100

    wip

--- year2019/day06/problem.go
+++ year2019/day06/problem.go
@@ -1,35 +1,77 @@
 package day06
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"strings"
 )
 
-type problem struct {}
+type problem struct{}
 
 func Problem() *problem {
 	return &problem{}
 }
 
 func (p problem) PartOneWithReader(r io.Reader) (string, error) {
-	_, err := p.parse(r)
+	orbits, err := p.parse(r)
 	if err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf("%d", 0), nil
+	var i int
+	for o := range orbits {
+		i += dist(orbits, o)
+	}
+
+	return fmt.Sprintf("%d", i), nil
 }
 
 func (p problem) PartTwoWithReader(r io.Reader) (string, error) {
-	_, err := p.parse(r)
+	orbits, err := p.parse(r)
 	if err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf("%d", 0), nil
+	y := orbits["YOU"]
+	s := orbits["SAN"]
+
+	return fmt.Sprintf("%d", dist(orbits, y)+dist(orbits, s)-2*dist(orbits, common(orbits, y, s))), nil
 }
 
-func (problem) parse(r io.Reader) ([]int, error) {
-	var es []int
-	return es, nil
+func common(m map[string]string, o1, o2 string) string {
+	if o1 == o2 {
+		return o1
+	}
+	s := make(map[string]struct{})
+	for c := m[o1]; c != "COM"; c = m[c] {
+		s[c] = struct{}{}
+	}
+	for c := m[o2]; c != "COM"; c = m[c] {
+		if _, ok := s[c]; ok {
+			return c
+		}
+	}
+	return "COM"
+}
+
+func dist(m map[string]string, o string) int {
+	i := 1
+	for c := m[o]; c != "COM"; c = m[c] {
+		i++
+	}
+	return i
+}
+
+func (problem) parse(r io.Reader) (map[string]string, error) {
+	orbits := make(map[string]string)
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		objects := strings.Split(s.Text(), ")")
+		if _, ok := orbits[objects[1]]; ok {
+			panic(objects[1])
+		}
+		orbits[objects[1]] = objects[0]
+	}
+	return orbits, nil
 }
--- year2019/day07/problem_test.go
+++ year2019/day07/problem_test.go
@@ -4,13 +4,33 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/SHyx0rmZ/advent-of-code/day07"
+	"github.com/SHyx0rmZ/advent-of-code/year2019/day07"
 )
 
 func TestProblem_PartOne(t *testing.T) {
-	r, err := day07.Problem().PartOneWithReader(strings.NewReader(""))
-	if r != "" || err != nil {
-		t.Errorf("got (%#v, %+v), want (%#v, %+v)", r, err, "", nil)
+	for _, tt := range []struct {
+		Input  string
+		Output string
+	}{
+		{
+			"3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
+			"4,3,2,1,0",
+		},
+		{
+			"3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+			"0,1,2,3,4",
+		},
+		{
+			"3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+			"1,0,4,3,2",
+		},
+	} {
+		t.Run(tt.Input, func(t *testing.T) {
+			r, err := day07.Problem().PartOneWithReader(strings.NewReader(tt.Input))
+			if r != tt.Output || err != nil {
+				t.Errorf("got (%#v, %+v), want (%#v, %+v)", r, err, tt.Output, nil)
+			}
+		})
 	}
 }
 
